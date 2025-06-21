84035519077d5ea9d8ea2409ebccadd39364088d
Finoana-Velia <fypatrick20@yahoo.com>
2025-06-21 16:33:05 +0300

    first commit

---
+++ src/main/java/com/Finoana/Bag/BagServerApplication.java
@@ -0,0 +1,13 @@
+package com.Finoana.Bag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BagServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BagServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Finoana/Bag/BagServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Finoana.Bag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BagServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
